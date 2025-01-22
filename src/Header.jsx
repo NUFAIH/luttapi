d421884edb44a86f9b888f70a0760ba66f8b8539
NUFAIH <nufaihmp@gmail.com>
2025-01-23 00:10:55 +0530

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+function Header(){
+
+    return(
+        <header>
+        <h1>Judiciary System Dashboard</h1>
+        <nav>
+          <ul>
+            <li>Cases</li>
+            <li>Parties</li>
+            <li>Hearings</li>
+            <li>Lawyers</li>
+            <li>Judges</li>
+          </ul>
+        </nav>
+      </header>
+
+    );
+}
+
+export default Header
